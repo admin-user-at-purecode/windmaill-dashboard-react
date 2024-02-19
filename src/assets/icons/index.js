f03918dbaf4694133bb611347f6095d2eac51846
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-19 18:13:59 +0530

    layoutupdated

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,17 +6,6 @@ import { ReactComponent as HomeIcon } from './home.svg'
 import { ReactComponent as ModalsIcon } from './modals.svg'
 import { ReactComponent as PagesIcon } from './pages.svg'
 import { ReactComponent as TablesIcon } from './tables.svg'
-import { ReactComponent as HeartIcon } from './heart.svg'
-import { ReactComponent as EditIcon } from './edit.svg'
-import { ReactComponent as TrashIcon } from './trash.svg'
-import { ReactComponent as ForbiddenIcon } from './forbidden.svg'
-import { ReactComponent as GithubIcon } from './github.svg'
-import { ReactComponent as TwitterIcon } from './twitter.svg'
-import { ReactComponent as MailIcon } from './mail.svg'
-import { ReactComponent as CartIcon } from './cart.svg'
-import { ReactComponent as ChatIcon } from './chat.svg'
-import { ReactComponent as MoneyIcon } from './money.svg'
-import { ReactComponent as PeopleIcon } from './people.svg'
 import { ReactComponent as SearchIcon } from './search.svg'
 import { ReactComponent as MoonIcon } from './moon.svg'
 import { ReactComponent as SunIcon } from './sun.svg'
@@ -36,17 +25,6 @@ export {
   ModalsIcon,
   PagesIcon,
   TablesIcon,
-  HeartIcon,
-  EditIcon,
-  TrashIcon,
-  ForbiddenIcon,
-  GithubIcon,
-  TwitterIcon,
-  MailIcon,
-  CartIcon,
-  ChatIcon,
-  MoneyIcon,
-  PeopleIcon,
   SearchIcon,
   MoonIcon,
   SunIcon,

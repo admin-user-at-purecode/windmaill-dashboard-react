3d478938612e7282c92d50a23a0e6333d42cb004
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-19 16:47:10 +0530

    layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Windmill } from '@windmill/react-ui';
+import Layout from './components/common/layout';
+
+function App() {
+  return (
+    <Windmill usePreferences>
+     <Layout>
+    </Layout>
+    </Windmill>
+  );
+}
+
+export default App;

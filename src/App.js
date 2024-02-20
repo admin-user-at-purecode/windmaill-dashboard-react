4e0fbbf325f03d357fcb06ee21e820bdfdbd2797
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-20 15:05:13 +0530

    tablecompleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Windmill } from '@windmill/react-ui';
 import Layout from './components/common/layout';
+import Tables from './pages/tables/Tables';
 
 function App() {
   return (
     <Windmill usePreferences>
      <Layout>
+      <Tables />
     </Layout>
     </Windmill>
   );
---
+++ src/pages/tables/Tables.js
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Table,
+  TableHeader,
+  TableCell,
+  TableBody,
+  TableRow,
+  TableFooter,
+  TableContainer,
+  Badge,
+  Avatar,
+  Button,
+  Pagination,
+} from '@windmill/react-ui'
+import { EditIcon, TrashIcon } from '../../assets/icons'
+
+import response from '../../components/table/tableData'
+import { PageTitle, SectionTitle } from '../../components/title/title'
+// make a copy of the data, for the second table
+const response2 = response.concat([])
+
+function Tables() {
+
+  const [pageTable1, setPageTable1] = useState(1)
+  const [pageTable2, setPageTable2] = useState(1)
+
+  // setup data for every table
+  const [dataTable1, setDataTable1] = useState([])
+  const [dataTable2, setDataTable2] = useState([])
+
+  // pagination setup
+  const resultsPerPage = 10
+  const totalResults = response.length
+
+  // pagination change control
+  function onPageChangeTable1(p) {
+    setPageTable1(p)
+  }
+
+  // pagination change control
+  function onPageChangeTable2(p) {
+    setPageTable2(p)
+  }
+
+  // on page change, load new sliced data
+  // here you would make another server request for new data
+  useEffect(() => {
+    setDataTable1(response.slice((pageTable1 - 1) * resultsPerPage, pageTable1 * resultsPerPage))
+  }, [pageTable1])
+
+  // on page change, load new sliced data
+  // here you would make another server request for new data
+  useEffect(() => {
+    setDataTable2(response2.slice((pageTable2 - 1) * resultsPerPage, pageTable2 * resultsPerPage))
+  }, [pageTable2])
+
+  
+function CTA() {
+  return (
+    <a
+      className="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple"
+    >
+      <div className="flex items-center">
+        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <span>Star this project on GitHub</span>
+      </div>
+      <span>
+        View more <span dangerouslySetInnerHTML={{ __html: '&RightArrow;' }}></span>
+      </span>
+    </a>
+  )
+}
+  return (
+    <>
+      <PageTitle>Tables</PageTitle>
+
+      <CTA />
+
+      <SectionTitle>Simple table</SectionTitle>
+      <TableContainer className="mb-8">
+        <Table>
+          <TableHeader>
+            <tr>
+              <TableCell>Client</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Date</TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {dataTable1.map((user, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <div className="flex items-center text-sm">
+                    <Avatar className="hidden mr-3 md:block" src={user.avatar} alt="User avatar" />
+                    <div>
+                      <p className="font-semibold">{user.name}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">{user.job}</p>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">$ {user.amount}</span>
+                </TableCell>
+                <TableCell>
+                  <Badge type={user.status}>{user.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">{new Date(user.date).toLocaleDateString()}</span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            onChange={onPageChangeTable1}
+            label="Table navigation"
+          />
+        </TableFooter>
+      </TableContainer>
+
+      <SectionTitle>Table with actions</SectionTitle>
+      <TableContainer className="mb-8">
+        <Table>
+          <TableHeader>
+            <tr>
+              <TableCell>Client</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Actions</TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {dataTable2.map((user, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <div className="flex items-center text-sm">
+                    <Avatar className="hidden mr-3 md:block" src={user.avatar} alt="User avatar" />
+                    <div>
+                      <p className="font-semibold">{user.name}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">{user.job}</p>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">$ {user.amount}</span>
+                </TableCell>
+                <TableCell>
+                  <Badge type={user.status}>{user.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">{new Date(user.date).toLocaleDateString()}</span>
+                </TableCell>
+                <TableCell>
+                  <div className="flex items-center space-x-4">
+                    <Button layout="link" size="icon" aria-label="Edit">
+                      <EditIcon className="w-5 h-5" aria-hidden="true" />
+                    </Button>
+                    <Button layout="link" size="icon" aria-label="Delete">
+                      <TrashIcon className="w-5 h-5" aria-hidden="true" />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            onChange={onPageChangeTable2}
+            label="Table navigation"
+          />
+        </TableFooter>
+      </TableContainer>
+    </>
+  )
+}
+
+export default Tables

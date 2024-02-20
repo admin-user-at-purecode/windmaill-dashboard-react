366d4f59cc4b6cca485455dc88072acb4ec27169
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-20 13:12:32 +0530

    modalcompleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Windmill } from '@windmill/react-ui';
 import Layout from './components/common/layout';
+import Modals from './pages/modal/Modals';
 
 function App() {
   return (
     <Windmill usePreferences>
      <Layout>
+      <Modals />
     </Layout>
-    </Windmill>
+    </Windmill
+    >
   );
 }
 

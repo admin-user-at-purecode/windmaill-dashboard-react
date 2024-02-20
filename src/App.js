9e683f3c45a8fca8ec41583da7bbd22abb1922cc
harsh-purecode <harsh.tiwari@purecodemarketplace.io>
2024-02-20 11:46:34 +0530

    chartsdone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Windmill } from '@windmill/react-ui';
 import Layout from './components/common/layout';
+import Charts from './pages/charts/Charts';
 
 function App() {
   return (
     <Windmill usePreferences>
      <Layout>
+      <Charts />
     </Layout>
     </Windmill>
   );

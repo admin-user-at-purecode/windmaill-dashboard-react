84dc7e5243bf730295d24086484aa2216ed3dde6
mahendhar-varma <7093741662mahiv@gmail.com>
2024-03-13 12:56:11 +0530

    main

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/tailwind.output.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
